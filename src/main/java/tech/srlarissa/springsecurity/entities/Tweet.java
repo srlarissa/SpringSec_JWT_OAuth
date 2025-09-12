af1594b1ef512da875d3982bc97d5b2021e608bd
LarIssa Soares Reis <larissasoaresreis@MacBook-Air-de-LarIssa.local>
2025-09-12 17:43:15 -0300

    feat(entities): Created Tweet class

---
+++ src/main/java/tech/srlarissa/springsecurity/entities/Tweet.java
@@ -0,0 +1,17 @@
+package tech.srlarissa.springsecurity.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_tweets")
+public class Tweet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long tweetId;
+    private User user;
+    private String content;
+    private Instant createdAt;
+}
